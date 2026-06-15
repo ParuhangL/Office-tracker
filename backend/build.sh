3275046a6ec2e87cce1c6ab904b262d74f1b3e4e
Paruhang Limbu <limbuparuhang1@gmail.com>
2026-06-15 16:25:28 +0545

    Weehoo2

--- backend/build.sh
+++ backend/build.sh
@@ -4,11 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-echo "
-from django.contrib.auth.models import User
-u = User.objects.get(username='Admin')
-u.set_password('password1234!')
-u.save()
-print('Password reset done')
-" | python manage.py shell
